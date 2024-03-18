092677d69913434574c205b6a2f7cfbecee2d14b
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-03-18 13:29:34 +0000

    a

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import request
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -13,25 +12,3 @@ def login():
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
-=======
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-# Define a route for the root URL
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Define a route for '/process' URL
-@app.route('/process', methods=['POST'])
-def process():
-    # Only execute this code when a POST request is made to '/process'
-    name = request.form.get('test_input')
-    # Process the data or perform any desired action
-    return f'Hello, {name}!'
-
-if __name__ == '__main__':
-    # Run the Flask application
-    app.run(debug=True)
->>>>>>> 6bed5d48b6dab314ff9b3e3eea49b9a814586496
