804d39a253dafb532d09a61a0cb0b2bb4a9b2727
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-04-02 03:03:24 +0000

    changement du python pour les "routes"

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import request, render_template, Flask
+from flask import request, render_template, Flask, redirect, url_for
 app = Flask(__name__)
 
 Users = {
     'admin': '123456',
 }
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('login.html')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = 'Erreur inconnue'
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return redirect(url_for('login'))
+    elif request.method == 'POST':
         Username = request.form['username']
         Passwd = request.form['passwd']
 
@@ -20,3 +17,7 @@ def login():
             return render_template('index.html', user=Username)
         else:
             return render_template('login.html', erreur='Identifiants invalides')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
