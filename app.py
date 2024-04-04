333ad58b743f5eacfa6f28b9a8cbcd27d3418f74
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-04-04 15:09:39 +0000

    Ajout du message d'erreur lorsque les informations de connections sont mauvaise

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import request, render_template, Flask, redirect, url_for, make_response, session
+from flask import request, render_template, Flask, redirect, url_for, session
 app = Flask(__name__)
 
-app.secret_key = b'36f81d8585f136a0536a350f69443bf8e26bcbc8407e8dc3bcf584bd92ba4cd6' # make sure to change this before using, and keep it secret (if you want to generate a secret one, you can use "python -c 'import secrets; print(secrets.token_hex())'")
+app.secret_key = b'36f81d8585f136a0536a350f69443bf8e26bcbc8407e8dc3bcf584bd92ba4cd6' # make sure to change this before using, and keep it secret (if you want to generate a secret one, you can use { python -c 'import secrets; print(secrets.token_hex())' } )
 
 Users = {
     'admin': '123456',
@@ -18,9 +18,9 @@ def index():
         if Username in Users and Users[Username] == Passwd:
             session['username'] = Username
             session['passwd'] = Passwd
-            render_template('index.html', user=Username)
+            return render_template('index.html')
         else:
-            return render_template('login.html', erreur='Identifiants invalides')
+            return render_template('login.html', error='Identifiants invalides')
 
 @app.route('/login')
 def login():
